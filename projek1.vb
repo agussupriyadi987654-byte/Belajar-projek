8aec4aa599670f239c8eb712b412efec9ffe3651
agussupriyadi987654-byte <agussupriyadi987654@gmail.com>
2026-06-07 15:43:45 +0700

    Update projek1.vb

--- projek1.vb
+++ projek1.vb
@@ -1,11 +1,26 @@
+Imports System
+
 Module Program
 Sub Main()
-	' Ini komentar
-	' ngga ada multibaris, sedih
-	 Console.WriteLine("Halo World") ' output
-	 Console.WriteLine("Output" & vbLf & "jg tidak ada" & vbLf & "multibaris")
+	'Ini komentar
+	'Ngga ada multibaris, sedih
+	Console.WriteLine("Halo") 'ini output
+	Console.WriteLine("Selamat" & vbLf & "Datang") 'ngga ada multibaris
 
-	 Dim var1 As Integer
-	 var1 = 14
-End sub
+	Dim var1 As String 'Variabel string
+	var1 = "Halo" 
+	Dim var2 As Integer
+	var2 = 14
+	Console.WriteLine(var1 & var2.ToString())
+	'Output variabel, ubah tipe data ke str
+	'.ToString() untuk VB Net
+	'CStr(var) untuk VBA
+	'Ngga bisa dibalik
+
+	Console.Write("Masukan Nama :") 'tanpa enter
+	Dim nama As String
+	nama = Console.ReadLine() 'input
+
+	Console.WriteLine("Halo " & nama)
+End Sub
 End Module
