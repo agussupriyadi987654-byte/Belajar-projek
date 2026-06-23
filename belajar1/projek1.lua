d5fe37953bd0074db427f58315d48054791c3691
Safi <agussupriyadi987654@gmail.com>
2026-06-23 18:20:30 +0700

    Update projek1.lua

--- belajar1/projek1.lua
+++ belajar1/projek1.lua
@@ -16,7 +16,7 @@ var1 = 10 -- ini variabel global
 local var2 = 15 -- ini variabel local
 var3 = "Halo" -- ini string
 
-print(var3..var1) -- pake pemisah .. bukan koma
+print(var3..var1) -- pake penghubung .. bukan +/&
 
 io.write("Masukan nama :") --output baris tetap
 nama = io.read()
